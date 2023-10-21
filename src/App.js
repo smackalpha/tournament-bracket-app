5fc969c721d84521e420c38f05df7a4d86710a8d
smackalpha <smackalpha@gmail.com>
2023-10-21 17:36:14 +0530

    Added Tournament Brackets Static

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./constants";
 
 function App() {
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="playoff-table">
+      <div className="playoff-table-content">
+
+        {/* Round 1 start */}
+        <div className="playoff-table-tour">
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+        </div>
+        {/* Round 2 start */}
+        <div className="playoff-table-tour">
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+        </div>
+        {/* Round 3 start */}
+        <div className="playoff-table-tour">
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+          </div>
+        </div>
+        {/* Round 4 start */}
+        <div className="playoff-table-tour">
+          <div className="playoff-table-group">
+            <div className="playoff-table-pair playoff-table-pair-style">
+              <div className="playoff-table-left-player">Иванов</div>
+              <div className="playoff-table-right-player">Сидоров</div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,62 @@
+export const data = {
+  round1: [
+    [1, "bye"],
+    [17, 16],
+    [9, "bye"],
+    ["bye", 8],
+    [5, "bye"],
+    ["bye", 12],
+    [13, 20],
+    ["bye", 4],
+    [3, "bye"],
+    [19, 14],
+    [11, "bye"],
+    ["bye", 6],
+    [7, "bye"],
+    ["bye", 10],
+    [15, 18],
+    ["bye", 2],
+  ],
+  round2: [
+    ["tbd", "tbd"],
+    [9, 8],
+    [5, 12],
+    ["tbd", "tbd"],
+    ["tbd", "tbd"],
+    [11, 6],
+    [7, 10],
+    ["tbd", "tbd"],
+  ],
+  round3: [
+    ["tbd", "tbd"],
+    ["tbd", "tbd"],
+    ["tbd", "tbd"],
+    ["tbd", "tbd"],
+  ],
+  round4: [
+    ["tbd", "tbd"],
+    ["tbd", "tbd"],
+  ],
+  round5: [["tbd", "tbd"]],
+};
+
+
+export const data2 = {
+    round1: [
+      [1, "bye"],
+      [17, 16],
+      [9, "bye"],
+      ["bye", 8],
+
+    ],
+    round2: [
+      ["tbd", "tbd"],
+      [9, 8],
+
+    ],
+    round3: [
+      ["tbd", "tbd"],
+
+    ]
+  };
+  
